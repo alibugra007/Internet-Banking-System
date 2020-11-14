1dd656193a8626e54ba3edd2c60261ca39191432
alibugra007 <61011204+alibugra007@users.noreply.github.com>
2020-11-14 18:01:53 +0300

    alibugra007

--- Internet Bankacılığı Proje/Internet Bankacılığı Proje/Form1.Designer.cs
+++ Internet Bankacılığı Proje/Internet Bankacılığı Proje/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(344, 102);
+            this.button1.Location = new System.Drawing.Point(68, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 142);
+            this.button1.Size = new System.Drawing.Size(137, 114);
             this.button1.TabIndex = 0;
             this.button1.Text = "ali bak şuna";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,21 +46,32 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(610, 162);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkBox1.Location = new System.Drawing.Point(662, 49);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(113, 24);
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 114);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "proje";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(924, 576);
+            this.ClientSize = new System.Drawing.Size(881, 462);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -70,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
